e3fa1783c385509b0da3ee89a418ab1a5856808b
김종권 <jongkwon.kim@42dot.com>
2021-08-11 22:50:56 +0900

    ex

---
+++ PhaseEx/Configurations/Environment.swift
@@ -0,0 +1,46 @@
+//
+//  Environment.swift
+//  PhaseEx
+//
+//  Created by 김종권 on 2021/08/11.
+//
+
+// Environment.swift
+
+import Foundation
+
+public enum Environment {
+    // MARK: - Keys
+    enum Keys {
+        enum Plist {
+            static let rootURL = "ROOT_URL"
+            static let apiKey = "API_KEY"
+        }
+    }
+
+    // MARK: - Plist
+    private static let infoDictionary: [String: Any] = {
+        guard let dict = Bundle.main.infoDictionary else {
+            fatalError("Plist file not found")
+        }
+        return dict
+    }()
+
+    // MARK: - Plist values
+    static let rootURL: URL = {
+        guard let rootURLstring = Environment.infoDictionary[Keys.Plist.rootURL] as? String else {
+            fatalError("Root URL not set in plist for this environment")
+        }
+        guard let url = URL(string: rootURLstring) else {
+            fatalError("Root URL is invalid")
+        }
+        return url
+    }()
+
+    static let apiKey: String = {
+        guard let apiKey = Environment.infoDictionary[Keys.Plist.apiKey] as? String else {
+            fatalError("API Key not set in plist for this environment")
+        }
+        return apiKey
+    }()
+}
---
+++ PhaseEx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhaseEx
+//
+//  Created by 김종권 on 2021/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
